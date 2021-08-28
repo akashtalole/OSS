8975dc56cfc2b263f9c57e22c77f80dc10b73967
root <root@rnd-akash02.enlight.dev>
2021-08-28 08:17:05 +0530

    Added gui App

--- oss/settings.py
+++ oss/settings.py
@@ -39,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+AVAILABLE_APPS = [
+	'gui',
+]
+
+#INSTALLED_APPS.extend(AVAILABLE_APPS)
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
